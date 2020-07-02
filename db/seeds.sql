3ba308aa9e2cfdce05cc5af5c4413078d28db3eb
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-07-01 17:20:37 -0700

    making the parts

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+USE burgers_db;
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("hamburger", false),
+    ("cheeseburger", false),
+    ("veggieburger", false);
+("Elkburger with ham and mushroom", TRUE)
--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+
+// Dependencies
+// =============================================================
 const express = require("express");
 
+// Sets up the Express App
+// =============================================================
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+require("./app/routes/html-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
