f0a7e77dd2680328d33b87e651e0ca77104e7a7d
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-07-06 14:47:14 -0700

    Update burger.js
    fixed syntax, standardized variables

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,24 @@
 const orm = require("../config/orm.js");
+
 const burger = {
-    selectAll: function (cb) {
+
+    All: function (cb) {
         orm.selectAll("burgers", function (res) {
             cb(res);
         });
     },
 
-    insertOne: function (cols, vals, cb) {
+    create: function (cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
 
-    updateOne: function (objColVals, condition, cb) {
+    update: function (objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function (res) {
             cb(res);
         });
-    },
-
-    deleteOne: function (condition, cb) {
-        orm.deleteOne("burgers", condition, function (res) {
-            cb(res);
-        });
     }
-};
+},
 
-module.exports = burger;
+    module.exports = burger;
