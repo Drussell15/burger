f0b146c7b6d9a81d644b8a020486698ea59b8a52
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-07-05 18:00:04 -0700

    upate routes
    cleaned up routes, because all over teh place due to inconsistent labels

--- db/models/controllers/burgers_controller.js
+++ db/models/controllers/burgers_controller.js
@@ -18,42 +18,32 @@ router.get("/", function (req, res) {
         res.render("index", burgerObj);
     });
 });
-router.post("/api/burger", function (req, res) {
-    burger.create([
-        "burger_name", "devoured"], [
-        req.body.burger_name, req.body.devoured
-    ], function (result) {
-        // Send back the ID of the new quote
+//post data to api
+
+router.post("/api/burgers", function (req, res) {
+    // create the new object
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        console.log(`result: ${result}`);
         res.json({ id: result.insertId });
     });
 });
+//upate is good
+
 router.put("/api/burger/:id", function (req, res) {
     const condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     burger.update({
-        burger_name: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
+        devoured: true
+    },
+        condition, function (result) {
+            if (result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
             res.status(200).end();
-        }
-    });
-});
-router.delete("/api/burger/:id", function (req, res) {
-    const condition = "id = " + req.params.id;
-
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+        });
 });
 
 // Export routes for server.js to use.
